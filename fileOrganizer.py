d5c2c8b4d17ab6d79d2c57921cbdc50f1272cf77
D'Aguila <34548216+daguila93@users.noreply.github.com>
2021-12-10 21:43:54 -0300

    Create fileOrganizer.py

---
+++ fileOrganizer.py
@@ -0,0 +1,23 @@
+from os import makedirs, mkdir, name, path
+from pathlib import Path
+import pathlib
+
+scriptName = 'fileOrganizer.py' #Script`s name to ignore
+currentPath = Path.cwd() #get the script`s current path
+
+def fileOrganizer(currentPath, scriptName):
+    #Create all the directories with the file`s extension
+    for child in currentPath.iterdir():
+        if(child.is_file() and child.name != scriptName):
+            fileExtension = child.suffix.replace('.', '')
+            pathlib.Path(currentPath).joinpath(fileExtension, '').mkdir(parents=True, exist_ok=True)
+
+    #Move all the files to their respectives folders
+    for child in currentPath.iterdir():
+        if (child.is_file() and child.name != scriptName):
+            fileExtension = child.suffix.replace('.', '')
+            fileFullpath = pathlib.Path(currentPath).joinpath(pathlib.Path(child).name)
+            fileDestination = pathlib.Path(currentPath).joinpath(fileExtension, pathlib.Path(child).name)
+            pathlib.Path.rename(fileFullpath, fileDestination)
+
+fileOrganizer(currentPath, scriptName)
